5653feefbd4f9f8149bb4e043c63142d6e3c9b01
清风不问烟雨 <aibayanyu@qq.com>
2020-07-22 15:11:23 +0800

    删除项目信息

--- config/jwt.php
+++
@@ -1,9 +0,0 @@
-<?php
-return [
-    'expire'=>7200,
-    'secret' => '9pgyDboySoemlOkws093sEXDV72OzxGj',
-    'iss'=>'',//签发者，可以为空
-    'aud'=>'',//面向的用户，可以空
-    'delay'=>0,//签发token延迟生效时间单位s
-    'refreshAuto'=>false //是否需要自动刷新token
-];
