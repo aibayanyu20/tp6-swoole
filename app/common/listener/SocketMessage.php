3408142203f52588c4bc8a5d27ec549fe19e3f08
清风不问烟雨 <aibayanyu@qq.com>
2020-07-30 11:54:44 +0800

    配置Socket配置项

---
+++ app/common/listener/SocketMessage.php
@@ -0,0 +1,17 @@
+<?php
+declare (strict_types = 1);
+
+namespace app\common\listener;
+
+class SocketMessage
+{
+    /**
+     * 事件监听处理
+     *
+     * @return mixed
+     */
+    public function handle($event)
+    {
+        //
+    }    
+}
