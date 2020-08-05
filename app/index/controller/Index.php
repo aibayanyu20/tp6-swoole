c0b94e3682e96386ec196972f31d7e02aea56a1c
清风不问烟雨 <aibayanyu@qq.com>
2020-08-05 09:04:46 +0800

    创建管理员账号

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -12,24 +12,26 @@ use app\common\model\Users;
 
 class Index extends BaseController
 {
-    public function index(){
+    public function index()
+    {
 //        $this->manager->getServer()->task([
 //            'data'=>'你好'
 //        ]);
         return "Sdsds";
     }
 
-    public function create(){
+    public function create()
+    {
         // 创建一个账号
         $arr = [
-            'nickname'=>'管理员账号',
-            'username'=>'admin',
-            'password'=>password_hash("admin",PASSWORD_DEFAULT),
-            'avatar'=>'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1316698706,2708023180&fm=11&gp=0.jpg',
-            'parent_id'=>0,
-            'phone'=>'13788881234',
-            'email'=>'aibayanyu@qq.com',
-            'status'=>1
+            'nickname' => '管理员账号',
+            'username' => 'admin',
+            'password' => password_hash("admin", PASSWORD_DEFAULT),
+            'avatar' => 'https://ss0.bdstatic.com/70cFvHSh_Q1YnxGkpoWK1HF6hhy/it/u=1316698706,2708023180&fm=11&gp=0.jpg',
+            'parent_id' => 0,
+            'phone' => '13788881234',
+            'email' => 'aibayanyu@qq.com',
+            'status' => 1
         ];
         (new Users())->save($arr);
     }
